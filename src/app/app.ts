dcb6ed69565d67bd1212ad767a7f4616e6b9b363
Bryan Santos <bryansantos@digix.com.br>
2026-02-13 23:27:35 -0300

    feat: estrutura inicial e paginas do app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./_components/navbar/navbar";
+import { CommonModule } from '@angular/common';
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
+import { CertificadoForm } from "./pages/certificado-form/certificado-form";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, CommonModule, BaseUi, Certificados, CertificadoForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('gerador-certificado');
+  exibeNavbar: boolean = false;
 }
